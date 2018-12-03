6bbde6002f83ff5af493f1866927e5ca52077692
xnng <xnng77@gmail.com>
2018-12-03 18:50:14 +0800

    add file upload

---
+++ nodejs-express-mongodb/README.md
@@ -0,0 +1,15 @@
+- [body-parser](https://github.com/expressjs/body-parser)
+  
+  处理 post 请求
+
+- Form 进行文件上传
+
+  要加上 `enctype="multipart/form-data"`
+
+- [multer](https://github.com/expressjs/multer)
+
+  文件上传
+
+- multer 文件上传
+
+  `upload.single("file")` 中 file 是上传框 name 值
--- nodejs-express-mongodb/server.js
+++ nodejs-express-mongodb/server.js
@@ -1,10 +1,50 @@
 var express = require("express");
+var bodyParser = require("body-parser");
+var fs = require("fs");
+
+// 文件上传
+var multer = require("multer");
+var storage = multer.diskStorage({
+  destination: function(req, file, cb) {
+    cb(null, __dirname + "/uploads");
+  },
+  filename: function(req, file, cb) {
+    cb(null, file.originalname);
+  }
+});
+var upload = multer({ storage: storage });
 
 var app = express();
+// 中间件，处理 post 提交的 json 格式的数据 (application/json)
+var jsonParser = bodyParser.json();
+// 中间件，处理 post 提交的 普通表单数据 (application/x-www-form-urlencoded)
+var urlencodedParser = bodyParser.urlencoded({ extended: false });
 
+// 路由参数
+// http://localhost:3000/test/123
+app.get("/test/:id", function(req, res) {
+  res.send(req.params.id);
+});
+
+//查询字符串
+// http://localhost:3000/?a=b
 app.get("/", function(req, res) {
-  res.send("this is the homepage");
+  res.send(req.query.a);
+});
+
+//普通表单的 post 请求
+app.post("/", urlencodedParser, function(req, res) {
+  res.send(req.body);
+});
+
+app.get("/form", function(req, res) {
+  var form = fs.readFileSync("./form.html", { encoding: "utf8" });
+  res.send(form);
+});
+
+app.post("/upload", upload.single("file"), function(req, res) {
+  res.send("upload successed");
 });
 
 app.listen(3000);
-console.log("listening to")
+console.log("server start at http://localhost:3000");
